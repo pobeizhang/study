f6f85657128cb66d7d41bde12f9c4ae60145855d
pobeizhang <dulei1618@163.com>
2017-11-03 16:00:40 +0800

    finished static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>@yield('title','ArleyDu App')</title>
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
