284c2589d11a883806ad528623a8d961358cbdb3
pobeizhang <dulei1618@163.com>
2018-01-02 01:56:44 +0000

    添加注释

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class StaticPagesController extends Controller
 {
+    /**
+     * 显示主页
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function home ()
     {
         $feed_items = [];
@@ -16,11 +22,21 @@ class StaticPagesController extends Controller
         return view( 'static_pages.home', compact( 'feed_items' ) );
     }
 
+    /**
+     * 显示帮助页面
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function help ()
     {
         return view( 'static_pages.help' );
     }
 
+    /**
+     * 显示关于页面
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function about ()
     {
         return view( 'static_pages.about' );
